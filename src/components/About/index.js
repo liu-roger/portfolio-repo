94e34d1649db0ac5ce6bc7bd27a4b8aef057ec20
Roger Liu <rogerliu80@gmail.com>
2022-11-12 15:11:11 -0500

    c

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,6 +3,7 @@ import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngular, faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
 
 const About = () => {
 
@@ -22,56 +23,60 @@ const About = () => {
     let aboutMe = 'About me'
     const aboutArray = aboutMe.split('')
     return (
-        <div className='container about-page'>
-            <div className='text-zone'>
-                <h1>
-                    <AnimatedLetters
-                        letterClass={letterClass}
-                        strArray={aboutArray}
-                        idx = {15}
-                    />
-                </h1>
-                <p>
-                    ieruoagihgaouigauiohgaougraluihgiuahgkluiagliua
-                </p>
+        <>
+            <div className='container about-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={aboutArray}
+                            idx = {15}
+                        />
+                    </h1>
+                    <p>
+                        ieruoagihgaouigauiohgaougraluihgiuahgkluiagliua
+                    </p>
 
-                <p>
-                    ieruoagihgaouigauiohgaougraluihgiuahgkluiagliua
-                </p>
+                    <p>
+                        ieruoagihgaouigauiohgaougraluihgiuahgkluiagliua
+                    </p>
 
-                <p>
-                    ieruoagihgaouigauiohgaougraluihgiuahgkluiagliua
-                </p>
-            </div>
+                    <p>
+                        ieruoagihgaouigauiohgaougraluihgiuahgkluiagliua
+                    </p>
+                </div>
 
-            <div className='stage-cube-cont'>
-                <div className='cubespinner'>
-                    <div className='face1'>
-                        <FontAwesomeIcon icon={faAngular} color="#DD0031"/>
-                    </div>
+                <div className='stage-cube-cont'>
+                    <div className='cubespinner'>
+                        <div className='face1'>
+                            <FontAwesomeIcon icon={faAngular} color="#DD0031"/>
+                        </div>
 
-                    <div className='face2'>
-                        <FontAwesomeIcon icon={faHtml5} color="#F06529"/>
-                    </div>
+                        <div className='face2'>
+                            <FontAwesomeIcon icon={faHtml5} color="#F06529"/>
+                        </div>
 
-                    <div className='face3'>
-                        <FontAwesomeIcon icon={faCss3} color="#28A4D9"/>
-                    </div>
+                        <div className='face3'>
+                            <FontAwesomeIcon icon={faCss3} color="#28A4D9"/>
+                        </div>
 
-                    <div className='face4'>
-                        <FontAwesomeIcon icon={faReact} color="#5ED4F4"/>
-                    </div>
+                        <div className='face4'>
+                            <FontAwesomeIcon icon={faReact} color="#5ED4F4"/>
+                        </div>
 
-                    <div className='face5'>
-                        <FontAwesomeIcon icon={faJsSquare} color="#EFD81D"/>
-                    </div>
+                        <div className='face5'>
+                            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D"/>
+                        </div>
 
-                    <div className='face6'>
-                        <FontAwesomeIcon icon={faGitAlt} color="#EC4D28"/>
+                        <div className='face6'>
+                            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28"/>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>)
+            <Loader type="pacman"/>
+        </>
+    )
 }
 
 export default About
